36522c5d30856f0ee05a407b1116414a82cc6008
Rohit <rohitsav1305@gmail.com>
2020-11-01 17:29:21 +0530

    Database added

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default = timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
     aurthor = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-# Create your views here.
-
-
-posts = [
-    {
-        'author':'Rohit Sav',
-        'title':'Blog title 1',
-        'content':'First post content',
-        'date_posted':'October 31 2020'
-    },
-    {
-        'author':'James',
-        'title':'Blog title 2',
-        'content':'Second post content',
-        'date_posted':'October 30 2020'
-    }
-]
+from . models import Post
 
 
 def home(request):
     context = {
-        'posts':posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
