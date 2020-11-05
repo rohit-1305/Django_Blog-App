0cef227091860e142d0bffb655c5b6f28a5c2de2
Rohit <rohitsav1305@gmail.com>
2020-11-05 21:35:24 +0530

    Login,Register and logout

--- users/views.py
+++ users/views.py
@@ -11,8 +11,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created For {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Hey {username} Your Account have been created and you are able to login !')
+            return redirect('login')
     else:
         # form = UserCreationForm()
         form = UserRegisterForm()
