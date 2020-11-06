7175d18f5497ebb52240314c07497f1dee856e67
Rohit <rohitsav1305@gmail.com>
2020-11-06 23:39:12 +0530

    ListView added to post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Post
+from django.views.generic import ListView
 
 
 def home(request):
@@ -7,6 +8,11 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
 
 
 def about(request):
