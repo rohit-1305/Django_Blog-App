a9478ff6ba9f13e783dc97d6f04371393eee6aff
Rohit <rohitsav1305@gmail.com>
2020-11-07 14:12:46 +0530

    Create,Update,Delete buttons added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . models import Post
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -31,19 +31,30 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)  # just running the form valid method to parent form
 
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.aurthor = self.request.user
         return super().form_valid(form)
-    
+
+    def test_func(self): # to check current post is the aurthor of the logged in person
+        post = self.get_object()
+        if self.request.user == post.aurthor:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.aurthor:
             return True
         return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About Page'})
