8738f12415f8ac7e469c4b4a68fc8162397f5518
Rohit <rohitsav1305@gmail.com>
2020-11-07 13:41:01 +0530

    Post Create

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from . models import Post
-from django.views.generic import ListView,DetailView
-
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
+
 
 class PostListView(ListView):
     model = Post
@@ -15,8 +16,19 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
     model = Post
 
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.aurthor = self.request.user
+        return super().form_valid(form)  # just running the form valid method to parent form
+
+
 def about(request):
-    return render(request,'blog/about.html',{'title':'About Page'})
+    return render(request, 'blog/about.html', {'title': 'About Page'})
