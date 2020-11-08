e29536a3adbc52f134ae65a41a7ccaa1946b6de2
Rohit <rohitsav1305@gmail.com>
2020-11-09 00:24:06 +0530

    Specific User Post Listed

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -15,8 +16,19 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+
     paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(aurthor=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -40,7 +52,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         form.instance.aurthor = self.request.user
         return super().form_valid(form)
 
-    def test_func(self): # to check current post is the aurthor of the logged in person
+    def test_func(self):  # to check current post is the aurthor of the logged in person
         post = self.get_object()
         if self.request.user == post.aurthor:
             return True
@@ -50,6 +62,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.aurthor:
