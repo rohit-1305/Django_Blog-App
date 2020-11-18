8708b385de5cb8c26ae7ecfaa2805376950eeeaf
Rohit <rohitsav1305@gmail.com>
2020-11-18 23:46:57 +0530

    new user added

--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, ** kwargs): #overriding the save method in model
-        super().save()
+    def save(self, *args, ** kwargs): #overriding the save method in model
+        super().save(*args, ** kwargs)
 
         img = Image.open(self.image.path) 
         if img.height > 300 or img.width > 300:
